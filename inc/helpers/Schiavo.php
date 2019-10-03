74868e7a7ee3027cba912c2bdb8de7fcd22e91b0
Hazuki Onohara <minecraft172zzz@gmail.com>
2019-10-03 18:28:31 +0700

    Uh, no.

--- inc/helpers/Schiavo.php
+++ inc/helpers/Schiavo.php
@@ -19,5 +19,5 @@ class Schiavo {
 // I don't remember. And don't wanna test to figure it out. So fuck it.
 function __schiavoCall($c, $message) {
 	global $schiavoConfig;
-	return file_get_contents($schiavoConfig["url"] . $c . "?message=" . urlencode("**old-frontend** " . $message));
+	return false
 }
